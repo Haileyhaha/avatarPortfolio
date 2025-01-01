ccbb33adfe0b2876ac757360747aeb585c44e06b
서혜인 <165886193+Haileyhaha@users.noreply.github.com>
2025-01-02 00:35:53 +0900

    Update Navbar.tsx
    Navbar 반응형 햄버거 메뉴 업데이트

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,25 +1,43 @@
+import { useState } from 'react';
 import logo from '../assets/portpoliologo.png';
 import { 
     NavbarContainer,
     NavbarContent,
     Logo,
     MenuLinks,
+    HamburgerMenu,
+    MobileMenu,
  } from '../styles/NavbarStyles';
 
-const Navbar: React.FC = () =>{
+const Navbar: React.FC = () => {
+    const [isOpen, setIsOpen] = useState(false); 
+
+    const toggleMenu = () => setIsOpen(!isOpen);
+
     return (
         <NavbarContainer>
             <NavbarContent>
                 <Logo className='mt-5'>
-                    <img src={logo} alt='Logo'/>
+                    <img src={logo} alt='Logo' />
                 </Logo>
-                <MenuLinks>
+                <HamburgerMenu onClick={toggleMenu} className='mt-3'>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </HamburgerMenu>
+                <MenuLinks className={isOpen ? 'active' : ''}>
                     <a href='/home'>Home</a>
                     <a href='/projects'>Projects</a>
                 </MenuLinks>
+                {isOpen && (
+                    <MobileMenu>
+                        <a href='/home'>Home</a>
+                        <a href='/projects'>Projects</a>
+                    </MobileMenu>
+                )}
             </NavbarContent>
         </NavbarContainer>
-    )
-}
+    );
+};
 
 export default Navbar;
